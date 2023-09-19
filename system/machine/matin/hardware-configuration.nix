c9415689a9a60ab3ee1c10758b0baee12befcaa5
Hossein Naderi <mail@hnaderi.dev>
2023-09-19 12:17:50 +0330

    Added kernel module for WIFI dongle

--- system/machine/matin/hardware-configuration.nix
+++ system/machine/matin/hardware-configuration.nix
@@ -4,25 +4,31 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "ata_generic" "uhci_hcd" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [
+    "ata_generic"
+    "uhci_hcd"
+    "ehci_pci"
+    "ahci"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+  ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.rtl88xxau-aircrack ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/c530b770-2b74-40b9-92eb-99fedbffb4cb";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/c530b770-2b74-40b9-92eb-99fedbffb4cb";
+    fsType = "ext4";
+  };
 
-  boot.initrd.luks.devices."luks-78f7db1a-4003-4ff3-ae0b-88155199ef44".device = "/dev/disk/by-uuid/78f7db1a-4003-4ff3-ae0b-88155199ef44";
+  boot.initrd.luks.devices."luks-78f7db1a-4003-4ff3-ae0b-88155199ef44".device =
+    "/dev/disk/by-uuid/78f7db1a-4003-4ff3-ae0b-88155199ef44";
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/359a3e9c-caa0-4749-b0ea-75d07e16e7a1"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/359a3e9c-caa0-4749-b0ea-75d07e16e7a1"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -32,7 +38,8 @@
   # networking.interfaces.enp0s25.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   nixpkgs.config.packageOverrides = pkgs: {
     vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
@@ -41,7 +48,7 @@
     enable = true;
     extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
       vaapiVdpau
       libvdpau-va-gl
     ];
