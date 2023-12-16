2dcfa8847a1560fe5526f02dc862d622bc5c197f
Hossein Naderi <hossein-naderi@hotmail.com>
2023-12-16 11:55:38 +0330

    enabled global dhcp

--- system/machine/m800/default.nix
+++ system/machine/m800/default.nix
@@ -74,9 +74,11 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.eno1.useDHCP = true;
-  networking.interfaces.wlp1s0.useDHCP = true;
+
+  networking.useDHCP = lib.mkDefault true;
+  # networking.useDHCP = false;
+  # networking.interfaces.eno1.useDHCP = true;
+  # networking.interfaces.wlp1s0.useDHCP = true;
 
   # nix.package = pkgs.nixUnstable;
 
