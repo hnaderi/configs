ecfdb2cf230d186cc057997531e1a756cd8d5504
Hossein Naderi <hossein-naderi@hotmail.com>
2024-08-01 19:01:57 +0330

    Removed no longer existing option

--- system/machine/behemoth/default.nix
+++ system/machine/behemoth/default.nix
@@ -49,12 +49,6 @@
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
-  # Enable sound.
-  sound = {
-    enable = true;
-    mediaKeys.enable = true;
-  };
-
   hardware.pulseaudio = {
     enable = true;
     package = pkgs.pulseaudioFull;
