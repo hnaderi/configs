7dc432337b9fdabdddf53f2db93f7c7c1b01ee91
Hossein Naderi <hossein-naderi@hotmail.com>
2025-02-20 13:46:26 +0330

    Added aarch64 emulation

--- system/machine/behemoth/default.nix
+++ system/machine/behemoth/default.nix
@@ -13,6 +13,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
