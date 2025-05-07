f50ea12cbb24245b46d86e846245a1d495c65093
Hossein Naderi <hossein-naderi@hotmail.com>
2025-05-07 12:20:37 +0330

    Nixified zebh script

--- home/base/scripts/default.nix
+++ home/base/scripts/default.nix
@@ -3,6 +3,14 @@
 let
   mkScript = name: pkgs.writeScriptBin name (builtins.readFile ./${name}.sh);
 
-  zebh = mkScript "zebh";
   dprox = mkScript "dprox";
+
+  kooshta = ./killed.wav;
+  zebh = pkgs.writeScriptBin "zebh" ''
+    #!/usr/bin/env sh
+
+    pkill "$@" && ${pkgs.alsa-utils}/bin/aplay -q ${kooshta}
+    exit $?
+  '';
+
 in { home.packages = [ zebh dprox ]; }
