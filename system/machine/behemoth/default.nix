08c00641370d1053c2a02a48821bcb01c39c03d1
Hossein Naderi <hossein-naderi@hotmail.com>
2025-09-05 15:36:05 +0330

    Replaced dnsmasq with dnscrypt

--- system/machine/behemoth/default.nix
+++ system/machine/behemoth/default.nix
@@ -97,9 +97,45 @@
 
   networking = {
     hostName = "behemoth";
-    networkmanager.enable = true;
+    nameservers = [ "127.0.0.1" "::1" ];
+    networkmanager = {
+      enable = true;
+      dns = "none";
+    };
   };
-  services.dnsmasq.enable = true;
+
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    # Settings reference:
+    # https://github.com/DNSCrypt/dnscrypt-proxy/blob/master/dnscrypt-proxy/example-dnscrypt-proxy.toml
+    settings = {
+      ipv6_servers = true;
+      require_dnssec = true;
+      # Add this to test if dnscrypt-proxy is actually used to resolve DNS requests
+      # query_log.file = "/var/log/dnscrypt-proxy/query.log";
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
+        minisign_key =
+          "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+
+      monitoring_ui = {
+        ## Enable the monitoring UI
+        enabled = true;
+        ## Listen address for the monitoring UI
+        listen_address = "127.0.0.1:8053";
+        username = "";
+      };
+
+      # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+      # server_names = [ ... ];
+    };
+  };
+
   services.v2raya = {
     enable = true;
     # cliPackage = pkgs.xray;
