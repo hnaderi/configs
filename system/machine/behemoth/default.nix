895c7e0d6835e121832d44c3671a2ad88e3746f9
Hossein Naderi <hossein-naderi@hotmail.com>
2025-10-21 14:17:37 +0330

    Fixed renamed service

--- system/machine/behemoth/default.nix
+++ system/machine/behemoth/default.nix
@@ -102,7 +102,7 @@
     };
   };
 
-  services.dnscrypt-proxy2 = {
+  services.dnscrypt-proxy = {
     enable = true;
     # Settings reference:
     # https://github.com/DNSCrypt/dnscrypt-proxy/blob/master/dnscrypt-proxy/example-dnscrypt-proxy.toml
