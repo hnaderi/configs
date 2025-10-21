b05f7907e12c99376e5585f882857fc50fb664c3
Hossein Naderi <hossein-naderi@hotmail.com>
2025-10-21 14:17:08 +0330

    Updated nvidia config

--- system/machine/behemoth/default.nix
+++ system/machine/behemoth/default.nix
@@ -85,10 +85,8 @@
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
-    powerManagement.finegrained = false;
     open = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   hardware.ledger.enable = true;
